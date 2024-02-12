5f63593b51ee5a178509396d2600978f7a07af0e
The17thDoctor <ewhorville@gmail.com>
2024-02-12 10:53:32 +0100

    Touch some grass

--- Assets/Game/Map/Scripts/Block.cs
+++ Assets/Game/Map/Scripts/Block.cs
@@ -8,17 +8,7 @@ public class Block : MonoBehaviour
     public bool Breakable;
 
     private bool _hidden = true;
-
-    [field:SerializeField]
-    public bool Hidden
-    {
-        get => _hidden;
-        set
-        {
-            _hidden = value;
-            GetComponent<SpriteRenderer>().sprite = value ? HiddenSprite : BlockSprite;
-        }
-    }
+    public bool Hidden { get => _hidden; }
 
     public Sprite BlockSprite;
     public Sprite HiddenSprite;
@@ -26,12 +16,24 @@ public class Block : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        
+        GetComponent<SpriteRenderer>().sprite = _hidden ? HiddenSprite : BlockSprite;
     }
 
     // Update is called once per frame
     private void Update()
     {
         
+    }
+
+    public void Reveal()
+    {
+        _hidden = false;
+        GetComponent<SpriteRenderer>().sprite = BlockSprite;
+    }
+
+    public void Hide()
+    {
+        _hidden = true;
+        GetComponent<SpriteRenderer>().sprite = HiddenSprite;
     }
 }
