e40426dfb3dc666ded2076c3f4787858b10dd21f
The17thDoctor <ewhorville@gmail.com>
2024-02-14 14:34:53 +0100

    JAI PUSHHHHHHH

--- Assets/Game/Map/Scripts/Block.cs
+++ Assets/Game/Map/Scripts/Block.cs
@@ -8,6 +8,8 @@ public class Block : MonoBehaviour
     private bool _hidden = true;
     public bool Hidden { get => _hidden; }
 
+    public int Health = 3;
+
     public Sprite BlockSprite;
     public Sprite HiddenSprite;
 
@@ -19,6 +21,19 @@ public class Block : MonoBehaviour
     public virtual void OnBlockBreak()
     {
         Debug.Log("KACER LE BLOKKKKKKKKKKKKKKKKK");
+    }
+
+    public bool Mine()
+    {
+        Health -= 1;
+        if (Health <= 0)
+        {
+            OnBlockBreak();
+            Destroy(gameObject);
+            return true;
+        }
+
+        return false;
     }
 
     public void Reveal()
--- Assets/Game/Map/Scripts/MapManager.cs
+++ Assets/Game/Map/Scripts/MapManager.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MapGenerator : MonoBehaviour
 {
@@ -27,11 +29,6 @@ public class MapGenerator : MonoBehaviour
         GenerateDirt();
         GenerateOres();
         GenerateBedrock();
-    }
-
-    private void Update()
-    {
-        
     }
 
     private void GenerateStone()
@@ -107,6 +104,34 @@ public class MapGenerator : MonoBehaviour
             if (Random.Range(0, 10) >= 5)
             {
                 SetBlock(HorizontalSize - 2, y, Instantiate(Bedrock));
+            }
+        }
+    }
+
+    public void MineBlock(int x, int y)
+    {
+        if (x <= 0 || y <= 0 || x > HorizontalSize || y > VerticalSize) return;
+
+        GameObject blockObject = BlockList[x, y];
+        if (blockObject == null) return;
+
+        Block block = blockObject.GetComponent<Block>();
+
+        Tuple<int, int>[] adjacents = {
+            new(1, 0),
+            new(-1, 0),
+            new(0, 1),
+            new(0, -1)
+        };
+        
+        // If block has been mined.
+        if (block.Mine())
+        {
+            foreach (Tuple<int, int> adjacent in adjacents)
+            {
+                GameObject adjacentObject = BlockList[x + adjacent.Item1, y + adjacent.Item2];
+                if (adjacentObject == null) continue;
+                adjacentObject.GetComponent<Block>().Reveal();
             }
         }
     }
