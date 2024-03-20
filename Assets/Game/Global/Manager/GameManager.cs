86eb45eda3be09e6ec0163d994873c16a57d6229
joffrey3 <fouche.joffrey@gmail.com>
2024-03-20 11:58:00 +0100

    Correction

--- Assets/Game/Global/Manager/GameManager.cs
+++ Assets/Game/Global/Manager/GameManager.cs
@@ -74,7 +74,7 @@ public class GameManager : MonoBehaviour
         if (time % timePeriod == 0)
         {
             ChangeSickPeople(time);
-            if (time > 15)
+            if (time > 150)
             {
                 ChangeDeadPeople(time);
             }
