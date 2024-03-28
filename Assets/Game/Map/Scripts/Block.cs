4b0f7db63925d11c39952cfe857525af047797a2
The17thDoctor <40616434+The17thDoctor@users.noreply.github.com>
2024-03-28 11:51:11 +0100

    Ca nicomine ici

--- Assets/Game/Map/Scripts/Block.cs
+++ Assets/Game/Map/Scripts/Block.cs
@@ -5,6 +5,9 @@ public class Block : MonoBehaviour
 
     public bool Breakable;
 
+    private bool _beingMined = false;
+    private float _amountMined = 0;
+
     private bool _hidden = true;
     public bool Hidden { get => _hidden; }
 
@@ -15,21 +18,58 @@ public class Block : MonoBehaviour
     public Sprite BlockSprite;
     public Sprite HiddenSprite;
 
+    private SpriteRenderer _renderer;
+    private float minDistort = 0.8f;
+    private float maxDistort = 1.2f;
+
     private void Start()
     {
-        GetComponent<SpriteRenderer>().sprite = _hidden ? HiddenSprite : BlockSprite;
+        _renderer = GetComponent<SpriteRenderer>();
+        _renderer.sprite = _hidden ? HiddenSprite : BlockSprite;
     }
 
     public virtual void OnBlockBreak()
     {
-        //Debug.Log("KACER LE BLOKKKKKKKKKKKKKKKKK");
+
     }
 
+    public void Update()
+    {
+        if (!_beingMined && _amountMined > 0)
+        {
+            _amountMined = Mathf.Max(_amountMined - Time.deltaTime / 2.0f, 0);
+        }
+
+        _beingMined = false;
+
+        _renderer.color = Color.Lerp(Color.white, Color.red, _amountMined / Health);
+
+        float distortX = Mathf.Lerp(1, Random.Range(minDistort, maxDistort), _amountMined / Health);
+        float distortY = Mathf.Lerp(1, Random.Range(minDistort, maxDistort), _amountMined / Health);
+        transform.localScale = new Vector3(distortX, distortY, 0);
+    }
+
+    /// <summary>
+    /// Call during Update()
+    /// </summary>
+    /// <returns></returns>
     public bool Mine()
+    {
+        _beingMined = true;
+        _amountMined += Time.deltaTime;
+
+        if (_amountMined >= Health)
+        {
+            Break();
+            return true;
+        }
+        return false;
+    }
+
+    public void Break()
     {
         OnBlockBreak();
         Destroy(gameObject);
-        return true;    
     }
 
     public void Reveal()
